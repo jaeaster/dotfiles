0f57759f28fc00c1f885f63e4a8e68b8d2c8859f
Jonathan Easterman <jonathaneasterman@gmail.com>
2020-04-05 14:31:28 -0700

    je - fix install script

--- brew_install.sh
+++ brew_install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-declare -a BREW_PACKAGES=(autojump exa neovim reattach-to-user-namespace ripgrep source-highlight fzf tmux)
-declare -a BREW_CASK_PACKAGEs=(alacritty amethyst karabiner-elements)
+declare -a BREW_PACKAGES=("autojump" "exa" "neovim" "reattach-to-user-namespace" "ripgrep" "source-highlight" "fzf" "tmux")
+declare -a BREW_CASK_PACKAGES=("alacritty" "amethyst" "karabiner-elements")
 
 brew update
 brew upgrade
 
-for pkg in $BREW_PACKAGES; do
+for pkg in "${BREW_PACKAGES[@]}"; do
 	echo $pkg
 	brew list $pkg || brew install $pkg
 done
 
-for pkg in $BREW_CASK_PACKAGES; do
+for pkg in "${BREW_CASK_PACKAGES[@]}"; do
 	echo $pkg
 	brew cask list $pkg || brew cask install $pkg
 done
